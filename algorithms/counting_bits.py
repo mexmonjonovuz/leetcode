d3cb711f78fbce5a5b4d46bdd4e1271bb0ee6ab8
mexmonjonovuz <asadbekmehmonjonov89@gmail.com>
2024-12-23 16:21:46 +0500

    best solution

--- algorithms/counting_bits.py
+++ algorithms/counting_bits.py
@@ -3,7 +3,30 @@ from typing import List
 
 
 def countBits(n: int) -> List[int]:
-    return [bin(i).count("1") for i in range(n + 1)]
+
+    # version 1
+    # result = []
+    # for i in range(n + 1):
+    #     result.append(bin(i).count("1"))
+    # return result
+
+    # version 2
+    # result = []
+    # i = 0
+    # while i <= n:
+    #     result.append(bin(i).count("1"))
+    #     i += 1
+    # return result
+
+    # version 3
+    # return [bin(i).count("1") for i in range(n + 1)]
+
+    # version 4
+    result = [0] * (n + 1)
+    for i in range(1, n + 1):
+        result[i] = result[i & (i - 1)] + 1
+    return result
+
 
 
 print(countBits(5))
