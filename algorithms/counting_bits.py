a6b1a630e78e9930db176ed02aa5f19eeefd62b5
mexmonjonovuz <asadbekmehmonjonov89@gmail.com>
2024-12-23 16:06:04 +0500

    count bits 3

--- algorithms/counting_bits.py
+++ algorithms/counting_bits.py
@@ -3,12 +3,7 @@ from typing import List
 
 
 def countBits(n: int) -> List[int]:
-    result = []
-    i = 0
-    while i <= n:
-        result.append(bin(i).count("1"))
-        i += 1
-    return result
+    return [bin(i).count("1") for i in range(n + 1)]
 
 
 print(countBits(5))
