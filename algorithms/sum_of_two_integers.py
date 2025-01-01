3a75b067fd2862dace5331cb3c5cdca5f4389567
mexmonjonovuz <asadbekmehmonjonov89@gmail.com>
2025-01-01 16:46:39 +0500

    as

---
+++ algorithms/sum_of_two_integers.py
@@ -0,0 +1,63 @@
+# # https://leetcode.com/problems/sum-of-two-integers/
+# def getSum(a: int, b: int) -> int:
+#     result = 1
+#     curr = a & b
+#     print(curr)
+#     xor_ab = a ^ b
+#     print(xor_ab)
+#     result >>= xor_ab ^ curr
+#
+#     print(result)
+#     return
+#
+#
+# print(getSum(4, 5))
+
+
+
+
+
+
+# hashable -> tuple, str, dict , float , int
+# unhashable -> dict , list, set ,
+
+# List of immutable types:
+
+# int, float, decimal, complex, bool, string, tuple, range, frozenset, bytes
+
+
+
+# List of mutable types:
+
+# list, dict, set, bytearray, user-defined classes
+
+
+
+# data class
+
+"""
+Data class nima ? . Data class __init__ constructorisiz class yaratish . Bunda  @dataclass decaratoridan foydalanga xolda class yaratilib class parametr sifatida berib yuborish mumkin .
+"""
+
+
+
+
+
+
+
+
+
+
+# pydantic ->
+
+
+
+
+
+
+
+
+
+
+
+
